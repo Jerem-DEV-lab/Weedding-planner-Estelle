5cc6e64c7c8e3af95f7c704583c6daf6274b63b3
Jeremy Guillemet <guillemet.jeremy087@gmail.com>
2021-03-29 14:24:51 +0200

    :sparkles: FEAT modification du schema utilisateur

---
+++ client/src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,61 @@
+import React, { useState }         from 'react'
+import { NavItems }                from './NavItems'
+import { NavLink }                 from 'react-router-dom'
+import { FaBars, FaTimes, FaUser } from 'react-icons/fa'
+
+const NavBar = () => {
+  const [navOpen, setNavOpen] = useState(false)
+  
+  const openNavBar = () => {
+    setNavOpen(!navOpen)
+  }
+  return <>
+    <div className={`overlay-nav ${navOpen ? 'modal-open' : ''}`} onClick={openNavBar}/>
+    <nav className="main-nav">
+      <div className="nav-brand">
+        <button className="nav-humburger" onClick={openNavBar}>
+          <FaBars size={25}/>
+        </button>
+      </div>
+      <div className="nav-logo">
+        <img src="/assets/logo.png" alt="Logo de côté campagne"/>
+      </div>
+      <div className={`nav-content ${navOpen ? 'modal-open' : ''}`}>
+        <div className="small-screen">
+          <div className="nav-close">
+            <button className="nav-btn-close" onClick={openNavBar}>
+              <FaTimes/>
+            </button>
+          </div>
+          <div className="nav-logo">
+            <img src="/assets/logo.png" alt="Logo de côté campagne"/>
+          </div>
+          <div className="nav-title">
+            <h3>Côté Campagne vous souhaite bienvenue !</h3>
+          </div>
+        </div>
+        <div className="nav-items">
+          <ul>
+            {NavItems.map(item => <>
+              <li>
+                <NavLink activeClassName="active" className="nav-link" to={item.url}>{item.label}</NavLink>
+              </li>
+            </>)}
+          </ul>
+        </div>
+        <div className="small-screen">
+          <div className="nav-social-link">
+            Mes réseaux
+          </div>
+        </div>
+      </div>
+      <div className="nav-item account-user">
+        <NavLink to="/login">
+          <span><FaUser/></span>Connexion / Inscription
+        </NavLink>
+      </div>
+    </nav>
+  </>
+}
+
+export default NavBar
