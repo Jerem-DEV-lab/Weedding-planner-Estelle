548875d504c40d8d1da9a41896c9cb849dad0693
Jeremy Guillemet <guillemet.jeremy087@gmail.com>
2021-03-29 14:24:51 +0200

    :sparkles: FEAT modification du schema utilisateur

---
+++ client/src/Components/NavBar/NavItems.js
@@ -0,0 +1,66 @@
+export const NavItems = [
+  {
+    label: 'Accueil',
+    url  : '/'
+  },
+  {
+    label  : 'Organisation',
+    url    : '/organisation',
+    subLink: [
+      {
+        label: 'Mariage',
+        url  : '/mariage',
+      },
+      {
+        label: 'Cérémonie laïque',
+        url  : '/ceremonie-laique',
+      },
+      {
+        label: 'EVJF / EVJG',
+        url  : '/enterrement-vie-jeune',
+      },
+      {
+        label: 'Baby Shower',
+        url  : '/baby-shower',
+      }, {
+        label: 'Autre évènements privés',
+        url  : '/events-private',
+      },]
+  },
+  {
+    label  : 'Mes ateliers',
+    url    : '/ateliers',
+    subLink: [
+      {
+        label: 'Mariage',
+        url  : '/mariage',
+      },
+      {
+        label: 'Cérémonie laïque',
+        url  : '/ceremonie-laique',
+      },
+      {
+        label: 'EVJF / EVJG',
+        url  : '/enterrement-vie-jeune',
+      },
+      {
+        label: 'Baby Shower',
+        url  : '/baby-shower',
+      }, {
+        label: 'Autre évènements privés',
+        url  : '/events-private',
+      }]
+  },
+  {
+    label: 'Galerie',
+    url  : '/galerie',
+  },
+  {
+    label: 'A propos',
+    url  : '/a-propos',
+  },
+  {
+    label: 'Contact',
+    url  : '/contact',
+  }
+]
