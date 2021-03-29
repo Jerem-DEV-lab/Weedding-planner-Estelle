d48e12cd9fb2037b2ad5af3b29e6584689808a79
Jeremy Guillemet <guillemet.jeremy087@gmail.com>
2021-03-29 10:55:32 +0200

    :adhesive_bandage: FIX / FEAT : Connexion utilisateur

--- client/src/Components/ModalAuth.jsx
+++ client/src/Components/ModalAuth.jsx
@@ -1,53 +1,100 @@
 import React, { useEffect, useState } from 'react'
 import { Link, useRouteMatch }        from 'react-router-dom'
+import { useDispatch, useSelector }   from 'react-redux'
+import { requestApiAuth }             from '../actions/authenticatorAction'
 
 const ModalAuth = () => {
-  const [stateForm, setStateForm] = useState('')
-  const useRoutesMatch            = useRouteMatch()
+  const [stateForm, setStateForm]       = useState('')
+  const useRoutesMatch                  = useRouteMatch()
+  const dispatch                        = useDispatch()
+  const stateLogin                      = useSelector(state => state.userReducers)
+  const [formRegister, setFormRegister] = useState(
+    {
+      civility       : '',
+      lastName       : '',
+      firstName      : '',
+      birthday       : '',
+      password       : '',
+      confirmPassword: '',
+      email          : '',
+      newsLetter     : false,
+    })
+  const [formLogin, setFormLogin]       = useState(
+    {
+      password: '',
+      email   : '',
+    })
   useEffect(() => {
     setStateForm(useRoutesMatch.path.split('/')[1])
   }, [useRoutesMatch])
-  const FormRegister = [
+  const FormRegister       = [
     { type: 'text', label: 'Nom : *', id: 'lastName', required: true },
-    { type: 'date', label: 'Prénom : *', id: 'firstName', required: true },
-    { type: 'text', label: 'Date de naissance : *', id: 'birthday', required: true },
+    { type: 'text', label: 'Prénom : *', id: 'firstName', required: true },
+    { type: 'date', label: 'Votre date d\'anniversaire : *', id: 'birthday', required: true },
     { type: 'password', label: 'Mot de passe : *', id: 'password', required: true },
     { type: 'password', label: 'Confirmez votre mot de passe : *', id: 'confirmPassword', required: true },
-    { type: 'email', label: 'email', required: true, id: 'email' }
+    { type: 'email', label: 'Email', required: true, id: 'email' }
   ]
-  const FormLogin    = [
+  const FormLogin          = [
     { type: 'text', label: 'Email : *', id: 'email' },
     { type: 'password', label: 'Mot de passe', id: 'password' },
   ]
+  const onChangeLogin      = (e) => {
+    setFormLogin({ ...formLogin, [e.target.id]: e.target.value })
+  }
+  const onChangeRegister   = (e) => {
+    if (e.target.type !== 'checkbox') {
+      setFormLogin({ ...formLogin, [e.target.id]: e.target.value })
+    } else {
+      if (e.target.id === 'Mr' || e.target.id === 'Mme') {
+        setFormRegister({ ...formRegister, civility: e.target.id })
+      } else {
+        setFormRegister({ ...formRegister, [e.target.id]: e.target.checked })
+      }
+    }
+  }
+  const submitFormLogin    = (e) => {
+    e.preventDefault()
+    dispatch(requestApiAuth(formLogin))
+  }
+  const submitFormRegister = (e) => {
+    e.preventDefault()
+    dispatch(requestApiAuth(formRegister))
+  }
   return <>
     <div className={`modal-auth ${stateForm}`}>
       <h1 className="h1 modal-title">Rejoindre Côté Campagne :</h1>
+      {stateLogin.isLoading && 'Chargement...'}
+      {stateLogin.userInfo.message && stateLogin.userInfo.message}
+      
       {stateForm === 'register' ?
-       <form className="form-register">
+       <form className="form-register" onSubmit={submitFormRegister}>
          <div className="grid-modal form-register">
            <fieldset className="form-group-civility">
              <legend className="col-form-label">Civilité : *</legend>
              <div className="form-check">
-               <input id="civility" type="checkbox" className="form-check-input"/>
-               <label htmlFor="civility" className="form-check-label">Mme</label>
+               <input id="Mme" type="checkbox" className="form-check-input"
+                      defaultChecked={formRegister.civility === 'Mme' ? 'true' : 'false'} onChange={onChangeRegister}/>
+               <label htmlFor="Mme" className="form-check-label">Mme</label>
              </div>
              <div className="form-check">
-               <input id="civility" type="checkbox" className="form-check-input"/>
-               <label htmlFor="civility" className="form-check-label">Mr</label>
+               <input id="Mr" type="checkbox" className="form-check-input" onChange={onChangeRegister}
+                      defaultChecked={formRegister.civility === 'Mr'}/>
+               <label htmlFor="Mr" className="form-check-label">Mr</label>
              </div>
            </fieldset>
            {FormRegister.map(input => (
              input.type === 'text' || 'date' || 'email' ? <>
                <div className={`form-group-${input.id}`}>
                  <label htmlFor={input.id} className="form-label">{input.label}</label>
-                 <input type={input.type} className="form-control"/>
+                 <input type={input.type} id={input.id} className="form-control" onChange={onChangeRegister}/>
                </div>
              </> : input.type === 'date'
            ))}
            <fieldset className="form-group-rgpd">
              <div className="form-check">
-               <input id="civility" type="checkbox" className="form-check-input"/>
-               <label htmlFor="civility" className="form-check-label">Accepter les <strong>
+               <input id="newsletter" type="checkbox" className="form-check-input" onChange={onChangeRegister}/>
+               <label htmlFor="rgpd" className="form-check-label">Accepter les <strong>
                  <a href="#">
                    conditions générale d'utilisation.
                  </a>
@@ -55,8 +102,8 @@ const ModalAuth = () => {
                </label>
              </div>
              <div className="form-check">
-               <input id="civility" type="checkbox" className="form-check-input"/>
-               <label htmlFor="civility" className="form-check-label">Recevoir la newsletters</label>
+               <input id="civility" type="checkbox" className="form-check-input" onChange={onChangeRegister}/>
+               <label htmlFor="newsletter" className="form-check-label">Recevoir la newsletters</label>
              </div>
            </fieldset>
            <div className="link-existing-account">
@@ -68,13 +115,13 @@ const ModalAuth = () => {
          <div className="btn-group btn-center">
            <button className="btn btn-primary hover-outline">S'inscrire</button>
          </div>
-       </form> : stateForm === 'login' && <form className="form-login">
+       </form> : stateForm === 'login' && <form className="form-login" onSubmit={submitFormLogin}>
         <div className="grid-modal form-login">
           {FormLogin.map(input => (
             <>
               <div className={`form-group-${input.id}`}>
                 <label htmlFor={input.id} className="form-label">{input.label}</label>
-                <input type={input.type} className="form-control"/>
+                <input type={input.type} className="form-control" id={input.id} onChange={onChangeLogin}/>
               </div>
             </>
           ))}
