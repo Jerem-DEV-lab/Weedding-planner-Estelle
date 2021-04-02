84e7750ada745e28635646e62c93c59364d3ce1d
Jeremy Guillemet <guillemet.jeremy087@gmail.com>
2021-04-02 18:20:51 +0200

    :sparkles: FEAT: Intégration de la page cérémonie laïque

--- client/src/Components/Button/Button.jsx
+++ client/src/Components/Button/Button.jsx
@@ -1,11 +1,11 @@
 import React    from 'react'
 import { Link } from 'react-router-dom'
 
-const Button = ({ isButton = true, label, color, onClick, className, type = 'submit' }) => {
+const Button = ({ isButton = true, size = '', label, color, onClick, className, type = 'submit' }) => {
   return <>
     {isButton ? <>
       <button className={`btn btn-${color} ${className}`} type={type} onClick={onClick}>{label}</button>
-    </> : <Link className={`btn btn-${color}`}>
+    </> : <Link className={`btn btn-${color} ${size ? `btn-${size}` : ''} ${className}`}>
        {label}
      </Link>}
   </>
