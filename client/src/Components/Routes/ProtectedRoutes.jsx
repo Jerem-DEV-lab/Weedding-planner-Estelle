f252d7faf8fbd9bfcbe0780e0847569d75adc5dd
Jeremy Guillemet <guillemet.jeremy087@gmail.com>
2021-04-06 14:32:11 +0200

    :sparkles: FEAT: Composant pour protéger les routes

---
+++ client/src/Components/Routes/ProtectedRoutes.jsx
@@ -0,0 +1,15 @@
+import React, { useContext } from 'react'
+import { UserContext }       from '../../Context/UserContext'
+import { Redirect, Route }   from 'react-router-dom'
+
+const ProtectedRoutes = ({ component: Component, ...rest }) => {
+  const { isLogged } = useContext(UserContext)
+  return <>
+    {!isLogged ? <Route {...rest} render={
+      props => <Component {...rest} {...props} />
+    }>
+    </Route> : <Redirect to="/"/>}
+  </>
+}
+
+export default ProtectedRoutes
