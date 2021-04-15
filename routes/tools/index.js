b797987d59ecec17bb25b868eb54330bb0cf05e8
Jeremy Guillemet <guillemet.jeremy087@gmail.com>
2021-04-15 14:31:59 +0200

    :sparkles: Ajout de la méthode pour générer des faux messages

--- routes/tools/index.js
+++ routes/tools/index.js
@@ -1,24 +1,54 @@
-const router     = require('express').Router()
-const UserSchema = require('../../db/Schema/UserSchema')
-const faker      = require('faker')
+const router        = require('express').Router()
+const UserSchema    = require('../../db/Schema/UserSchema')
+const ContactSchema = require('../../db/Schema/ContactSchema')
+const faker         = require('faker')
+
+/*function createRouterTest () {
+ const roles = ['ROLE_USER']
+ router.post('/fake-data', async (req, res) => {
+ try {
+ for (let i = 1; i < 10; i++) {
+ const user = new UserSchema(
+ {
+ firstName : faker.name.firstName(),
+ lastName  : faker.name.lastName(),
+ email     : faker.internet.email(),
+ phone     : faker.phone.phoneNumber('0695415462'),
+ password  : '123456',
+ civility  : 'Mme',
+ address   : faker.address.cityName(),
+ postalCode: faker.address.zipCode('21490'),
+ birthday  : faker.date.past('2021', ''),
+ roles     : roles
+ })
+ await user.save()
+ }
+ return res.status(200).send('Data correctement générer')
+ } catch (e) {
+ console.log('err', e)
+ }
+ })
+ 
+ return router
+ }*/
+
+function getRandomInt (max) {
+  return Math.floor(Math.random() * max)
+}
 
 function createRouterTest () {
-  const roles = ['ROLE_USER']
   router.post('/fake-data', async (req, res) => {
     try {
       for (let i = 1; i < 10; i++) {
-        const user = new UserSchema(
+        const user = new ContactSchema(
           {
-            firstName : faker.name.firstName(),
-            lastName  : faker.name.lastName(),
-            email     : faker.internet.email(),
-            phone     : faker.phone.phoneNumber('0695415462'),
-            password  : '123456',
-            civility  : 'Mme',
-            address   : faker.address.cityName(),
-            postalCode: faker.address.zipCode('21490'),
-            birthday  : faker.date.past('2021', ''),
-            roles     : roles
+            firstName       : faker.name.firstName(),
+            lastName        : faker.name.lastName(),
+            email           : faker.internet.email(),
+            numberOfGuests  : getRandomInt(10),
+            eventDep        : '123456',
+            organizationName: 'Mme',
+            message         : "A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart. I am alone, and feel the charm of existence in this spot, which was.",
           })
         await user.save()
       }
