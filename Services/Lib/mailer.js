0e01cdfc04db4908c46e39ebc0d2b2e863ffea7b
Jeremy Guillemet <guillemet.jeremy087@gmail.com>
2021-05-23 12:30:04 +0200

    :adhesive_bandage: FIX: Remplacement de nodemailer / sendgrid

--- Services/Lib/mailer.js
+++ Services/Lib/mailer.js
@@ -1,17 +1,23 @@
-const nodemailer = require('nodemailer')
-
-module.exports = transporter = nodemailer.createTransport(
-  {
-    service   : 'gmail',
-    auth      : {
-      user: "test.dev.node@gmail.com",
-      pass: "yf1Nr53X"
-    },
-  })
-
-transporter.verify((err) => {
-  if (err) {
-    console.log(err)
+const sgMail               = require('@sendgrid/mail')
+const templateMailSendGrid = require('../../Services/Lib/templateMail')
+module.exports = transporter = function sendMailWithSG (receiverEmail, nameUser, subjectMessage) {
+  sgMail.setApiKey('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')
+  const obj = {
+    'personalizations': [{
+      'to'     : [{ 'email': receiverEmail, 'name': 'John Doe' }],
+      'subject': subjectMessage
+    }],
+    'content'         : [{ 'type': 'text/html', 'value': templateMailSendGrid }],
+    'from'            : { 'email': 'estelle-rouille@estelle-events.fr', 'name': nameUser },
+    'reply_to'        : { 'email': 'guillemet.jeremy087@gmail.com', 'name': 'estelle events' },
   }
-  console.log('envoi de mail possible')
-})
+  sgMail
+    .send(obj)
+    .then(() => {console.log("envoyer")}, error => {
+      console.error(error)
+      
+      if (error.response) {
+        console.error(error.response.body)
+      }
+    })
+}
