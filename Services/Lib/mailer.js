901b204ef1d31ed90a07a9f20f4d1a912703817e
Jeremy Guillemet <guillemet.jeremy087@gmail.com>
2021-05-23 19:29:46 +0200

    :sparkles: FEAT: Changement de la clé API sendGRid

--- Services/Lib/mailer.js
+++ Services/Lib/mailer.js
@@ -1,7 +1,7 @@
 const sgMail               = require('@sendgrid/mail')
 const templateMailSendGrid = require('../../Services/Lib/templateMail')
 module.exports = transporter = function sendMailWithSG (receiverEmail, nameUser, subjectMessage) {
-  sgMail.setApiKey('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')
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY)
   const obj = {
     'personalizations': [{
       'to'     : [{ 'email': receiverEmail, 'name': 'John Doe' }],
@@ -13,7 +13,29 @@ module.exports = transporter = function sendMailWithSG (receiverEmail, nameUser,
   }
   sgMail
     .send(obj)
-    .then(() => {console.log("envoyer")}, error => {
+    .then(() => {console.log('envoyer')}, error => {
+      console.error(error)
+      
+      if (error.response) {
+        console.error(error.response.body)
+      }
+    })
+}
+
+module.exports = sendMailSG = async (receiverEmail, nameUser, subjectMessage, templateEmail) => {
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+  const obj = {
+    'personalizations': [{
+      'to'     : [{ 'email': receiverEmail, 'name': 'estelle-events.fr' }],
+      'subject': subjectMessage
+    }],
+    'content'         : [{ 'type': 'text/html', 'value': templateEmail }],
+    'from'            : { 'email': 'estelle-rouille@estelle-events.fr', 'name': nameUser },
+    'reply_to'        : { 'email': 'estelle.rouille.events@gmail.com', 'name': 'estelle events' },
+  }
+  sgMail
+    .send(obj)
+    .then(() => {console.log('envoyer')}, error => {
       console.error(error)
       
       if (error.response) {
